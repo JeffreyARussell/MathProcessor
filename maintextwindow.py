8ed111e9b0ef37fb99d4892d1119ea4638291c7f
JeffreyAR <jar8@email.sc.edu>
2024-07-05 23:16:06 -0700

    chore: Remove old code

---
+++ maintextwindow.py
@@ -0,0 +1,66 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeyEvent, QTextCursor, QKeySequence
+from PyQt6.QtWidgets import QTextEdit
+from configservice import get_character_from_shortcut, validate_config
+
+WHITESPACE_KEYS = [
+    Qt.Key.Key_Tab.value,
+    Qt.Key.Key_Return.value,
+    Qt.Key.Key_Enter.value,
+    Qt.Key.Key_Space.value
+]
+SHORTCUT_START_CHAR = '/'
+SHORTCUT_START_KEY = QKeySequence.fromString(SHORTCUT_START_CHAR)[0].key().value
+CODE_TERMINATER_KEYS = [
+    *WHITESPACE_KEYS,
+    Qt.Key.Key_Slash.value
+]
+RESET_KEYS = [
+    Qt.Key.Key_Left.value,
+    Qt.Key.Key_Right.value,
+    Qt.Key.Key_Up.value,
+    Qt.Key.Key_Down.value
+]
+MAX_SEARCH_DISTANCE = 4
+
+class MainTextWindow(QTextEdit):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+        self.initLogic()
+
+    def initUI(self):
+        return
+    
+    def initLogic(self):
+        self.trackingCode = False
+        self.currentCode = []
+
+    def resetLogic(self):
+        self.trackingCode = False
+        self.currentCode = []
+    
+    def keyPressEvent(self, e) -> None:
+        cursor = self.textCursor()
+        if e.key() not in CODE_TERMINATER_KEYS:
+            return super().keyPressEvent(e)
+        for i in range(MAX_SEARCH_DISTANCE):
+            cursor.movePosition(QTextCursor.MoveOperation.Left, QTextCursor.MoveMode.KeepAnchor)
+            selection = cursor.selectedText()
+            if not selection.startswith(SHORTCUT_START_CHAR):
+                continue
+
+            selection = selection[1:]
+            character = get_character_from_shortcut(selection)
+            if not character is None:
+                cursor.insertText(character)
+            break
+
+        return super().keyPressEvent(e)
+    
+    def replaceShortcutWithCharacter(self, character):
+        if character is None:
+            return
+        self.append(character)
